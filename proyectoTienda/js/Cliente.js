8a3f4a835dc7701eb2eb2209bf793c39248c0cb8
YagoRayon <113517995+YagoRayon@users.noreply.github.com>
2022-11-07 10:17:53 +0100

    Add files via upload

--- proyectoTienda/js/Cliente.js
+++ proyectoTienda/js/Cliente.js
@@ -1,3 +1,21 @@
 class Cliente{
-    constructor(id,nombre,dni,usuario,password,imagen,)
+    constructor(dni,nombre,usuario,password,correo,rutaImagen){
+        this.DNI=dni || 'Sin DNI';
+        this.nombre=nombre || 'Sin Nombre';
+        this.usuario=usuario || 'Sin Usuario';
+        this.password=password || 'Sin Contraseña';
+        this.correo=correo || 'Sin Correo';
+        this.rutaImagen=rutaImagen || 'sinRuta.png';
+        this.comprasPasadas = [];
+    }
+
+    cambiarImagen(nuevaRuta){
+        this.rutaImagen=nuevaRuta;
+    }
+    cambiarPassword(nuevaPass){
+        this.password=nuevaPass;
+    }
+    addCompra(idCompra){
+        this.comprasPasadas.push(idCompra);
+    }
 }
